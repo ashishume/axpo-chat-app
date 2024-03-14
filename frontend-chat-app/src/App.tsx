88581ce75a2363ef29f6b16ba6e1a1953d8e4ea5
Ashish Debnath <ashishume@gmail.com>
2024-03-14 17:49:56 +0530

    feat: chat support added

--- frontend-chat-app/src/App.tsx
+++ frontend-chat-app/src/App.tsx
@@ -1,39 +1,7 @@
 import { useEffect, useRef } from "react";
 import io from "socket.io-client";
-import Router from "./routing/routes";
 
 const ChatComponent = () => {
-  const socket = useRef(null as any);
-
-  useEffect(() => {
-    // Establish a socket connection
-    socket.current = io("http://localhost:9000"); // Replace with your server URL
-
-    // Event listeners
-    socket.current.on("connect", (e: any) => {
-      console.log(e, "Connected to server");
-      socket.current.emit("login", "user123");
-    });
-
-    socket.current.on("disconnect", (e: any) => {
-      console.log(e, "Disconnected from server");
-    });
-    socket.current.on("message", (msg: any) => {
-      console.log(msg);
-    });
-    // Clean up
-    return () => {
-      socket.current.disconnect();
-    };
-  }, []);
-  const handleClick = () => {
-    let messageObj = {
-      senderId: "user123",
-      targetId: "randomUser123",
-      message: "Hey there im here",
-    };
-    socket.current.emit("message", messageObj);
-  };
   return (
     <>
       {/* <button type="button" onClick={handleClick}>
--- frontend-chat-app/src/pages/Home/index.tsx
+++ frontend-chat-app/src/pages/Home/index.tsx
@@ -1,13 +1,84 @@
+import { useRef, useEffect, useState } from "react";
 import useLocalStorage from "../../shared/Hooks/useLocalStorage";
-
+import io from "socket.io-client";
+import "./style.scss";
+import axios from "axios";
 const Home = () => {
-  const { removeStoredValue } = useLocalStorage("auth");
+  const { value, removeStoredValue } = useLocalStorage("auth");
+  const [message, setMessage] = useState("");
+  const [chatMessages, setChatMessages] = useState([] as any);
+  const [users, setUsers] = useState([]);
+  const socket = useRef(null as any);
+  useEffect(() => {
+    socketConnection();
+
+    const url = `${import.meta.env.VITE_BASE_URL}/users`;
+    axios.get(url).then((res) => {
+      if (res.status === 200) {
+        setUsers(res.data);
+      }
+    });
+
+    return () => {
+      socket.current.disconnect();
+    };
+  }, []);
+
   const logOut = () => {
     removeStoredValue("auth");
   };
+
+  const socketConnection = () => {
+    // Establish a socket connection
+    socket.current = io("http://192.168.1.7:9000"); // Replace with your server URL
+
+    // Event listeners
+    socket.current.on("connect", (e: any) => {
+      console.log(e, "Connected to server");
+      socket.current.emit("login", value?.id);
+    });
+
+    socket.current.on("disconnect", (e: any) => {
+      console.log(e, "Disconnected from server");
+    });
+    socket.current.on("message", (msg: any) => {
+      console.log(msg);
+
+      setChatMessages((prev: any) => [...prev, msg]);
+    });
+  };
+
+  const handleClick = () => {
+    let messageObj = {
+      senderId: value?.id,
+      targetId: (users.find((v: any) => v?.id !== value?.id) as any).id,
+      message,
+    };
+    socket.current.emit("message", messageObj);
+  };
+
   return (
-    <div>
+    <div className="chat-container">
       <button onClick={logOut}>Logout</button>
+      <div className="chat-content">
+        <div className="chat-messages">
+          {chatMessages.map(({ message }: any) => {
+            return (
+              <div key={Math.floor(Math.random() * 100000)}>{message}</div>
+            );
+          })}
+        </div>
+        <input
+          type="text"
+          className="chat-input"
+          value={message}
+          placeholder="Enter your message..."
+          onChange={(e: any) => setMessage(e.target.value)}
+        />
+        <button onClick={handleClick} className="submit-btn">
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
